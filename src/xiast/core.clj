18d6398c4ceb960316a6787a0d325d231c10a3b6
Spartam <kwpardon@infogroep.be>
2013-12-01 21:28:05 +0100

    Fixes #6 Basic layout page css has yet to be added mostly serves has an example
    -> layout with div that should be extended
    html pages, extract snippets
    place snippets inside the layout page

--- src/xiast/core.clj
+++ src/xiast/core.clj
@@ -7,14 +7,35 @@
             [compojure.response :as response]
             [xiast.query :as query]))
 
-(deftemplate main-page "templates/index.html"
-  [courses]
+
+(defsnippet index-page-body "templates/index.html" [:#page-content] []
+  identity)
+
+(defsnippet about-page-body "templates/about.html" [:#page-content] []
+  identity)
+
+(defsnippet login-page-body "templates/login.html" [:#page-content] []
+  identity)
+
+(deftemplate index-page "templates/layout.html"
+  [courses index]
+  [:div#page-content] (content index)
   [:ul#course-list :li] (clone-for [course courses]
                                    (content (:title course))))
 
+(deftemplate about-page "templates/layout.html"
+  [about]
+  [:div#page-content] (content about))
+
+(deftemplate login-page "templates/layout.html"
+  [login]
+  [:div#page-content] (content login))
+
 ;;; Read: https://github.com/weavejester/compojure/wiki
 (defroutes main-routes
-  (GET "/" [] (main-page (query/courses *mock-data*)))
+  (GET "/" [] (index-page (query/courses *mock-data*) (index-page-body)))
+  (GET "/about" [] (about-page (about-page-body)))
+  (GET "/login" [] (login-page (login-page-body)))
   (route/not-found "Not found!"))
 
 (def app
