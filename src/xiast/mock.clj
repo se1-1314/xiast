2e0f356f098cd525d706c3bf6f3ed13ed7ccbe82
Adriaan Leijnse <adriaan@leijnse.net>
2013-12-01 17:35:30 +0100

    Query API and mock data implementation are complete

--- src/xiast/mock.clj
+++ src/xiast/mock.clj
@@ -1,17 +1,59 @@
 (ns xiast.mock
   (:use xiast.query))
 
-(defrecord MockData [courses])
+(defrecord MockData [courses schedules enrollment])
 
 (def *mock-data*
-  (MockData. [{:id "1" :title "Mathematics"}
-              {:id "2" :title "Physics"}
-              {:id "3" :title "Scheme"}]))
+  (MockData. {"course1" {:title "Mathematics"}
+              "course2" {:title "Physics"}
+              "course3" {:title "Scheme"}}
+             {"course1" {1 {1 [{:time [3 6]
+                                :room "E1.01"}]
+                            2 [{:time [7 12]
+                                :room "E1.02"}]}}}
+             {"student1" #{"course1"}}))
+
+(defn- schedule-blocks [{schedules :schedules
+                         courses :courses}
+                        course-id]
+  (let [course {:id course-id
+                :title (-> courses (get course-id) :title)}]
+    (for [week (get schedules course-id)
+          day (val week)
+          block (val day)]
+      {:week (key week)
+       :day (key day)
+       :start-time (first (:time block))
+       :end-time (second (:time block))
+       :course course
+       :room (:room block)})))
+
+(defn- filter-timespan [timespan schedule-blocks]
+  (filter #(xiast.query/schedule-block-in-timespan? % timespan)
+          schedule-blocks))
 
 (extend-type MockData
-  xiast.query/CourseList
+  xiast.query/XiastQuery
   (courses
     ([this]
        (:courses this))
     ([this title-kw]
-       (filter #(.contains (:title %) title-kw) (:courses this)))))
+       (filter #(.contains (:title %) title-kw) (:courses this))))
+  (course-schedule
+    ([this course-id]
+       (schedule-blocks this course-id))
+    ([this course-id timespan]
+       (filter-timespan timespan (course-schedule this course-id))))
+  (student-schedule
+    ([this student-id]
+       (mapcat #(schedule-blocks this %)
+               (get-in *mock-data* [:enrollment student-id])))
+    ([this student-id timespan]
+       (filter-timespan timespan (student-schedule this student-id))))
+  (room-schedule
+    ([this room-id]
+       (->> (keys (:schedules this))
+            (mapcat #(schedule-blocks this %))
+            (filter #(= (:room %) room-id))))
+    ([this room-id timespan]
+       (filter-timespan timespan (room-schedule this room-id)))))
