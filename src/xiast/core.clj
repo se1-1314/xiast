86a0c599a9fd80201f860a806ab16ce9e7d5d11c
Adriaan Leijnse <adriaan@leijnse.net>
2013-12-06 23:27:18 +0100

    Refactored routes and templates in core.clj

--- src/xiast/core.clj
+++ src/xiast/core.clj
@@ -1,12 +1,12 @@
 (ns xiast.core
   (:use compojure.core
-        net.cgrand.enlive-html
         [xiast.mock :only [*mock-data*]]
         [xiast.session :as session]
         [xiast.authentication :as auth]
         [ring.middleware.file-info :only [wrap-file-info]]
         [ring.middleware.resource :only [wrap-resource]]
-        [ring.handler.dump :only [handle-dump]])
+        [ring.handler.dump :only [handle-dump]]
+        net.cgrand.enlive-html)
   (:require [compojure.route :as route]
             [compojure.handler :as handler]
             [compojure.response :as response]
@@ -17,50 +17,53 @@
             [xiast.translate :as translate]))
 
 
-(defsnippet index-page-body "templates/index.html" [:#page-content] []
-  identity)
+(deftemplate base "templates/layout.html"
+  [body]
+  [:div#page-content] (content body))
 
-(defsnippet about-page-body "templates/about.html" [:#page-content] []
-  identity)
-
-(defsnippet login-page-body "templates/login.html" [:#page-content] []
-  identity)
-
-(deftemplate index-page "templates/layout.html"
-  [courses index]
-  [:div#page-content] (content index)
-  [:ul#course-list :li] (clone-for [course courses]
+(defsnippet index-body "templates/index.html" [:div#page-content]
+  []
+  [:ul#course-list :li] (clone-for [course (query/courses *mock-data*)]
                                    (content (:title (val course)))))
+(defroutes index-routes
+  (GET "/" [] (base (index-body))))
 
-(deftemplate about-page "templates/layout.html"
-  [about]
-  [:div#page-content] (content about))
 
-(deftemplate login-page "templates/layout.html"
-  [login]
-  [:div#page-content] (content login))
+(defsnippet about-body "templates/about.html" [:div#page-content]
+  []
+  identity)
+(defroutes about-routes
+  (GET "/about" [] (base (about-body))))
 
-;;; Read: https://github.com/weavejester/compojure/wiki
-(defroutes main-routes
-  (GET "/" [] (index-page (query/courses *mock-data*) (index-page-body)))
-  (GET "/about" [] (about-page (about-page-body)))
+(defsnippet login-body "templates/login.html" [:div#page-content]
+  []
+  identity)
+
+(defroutes login-routes
   (GET "/login" [] (login-page (login-page-body)))
   (POST "/login" {cookies :cookies params :params}
     (let [res (auth/login (:user params) (:pwd params))]
       (if res
         {:body (index-page (query/courses *mock-data*) (index-page-body))
          :cookies (session/to-cookies res)}
-        (login-page (login-page-body)))))
+        (base (login-body)))))
   (GET "/logout" {cookies :cookies}
     (let [session (session/from-cookies cookies)]
       ;; TODO: Redirect instead of directly showing index page
-      {:body (index-page (query/courses *mock-data*) (index-page-body))
-       :cookies (session/to-cookies (session/kill-session! session))}))
+      {:body (base (index-body))
+       :cookies (session/to-cookies (session/kill-session! session))})))
+
+
+;;; Read: https://github.com/weavejester/compojure/wiki
+(defroutes main-routes
+  index-routes
+  about-routes
+  login-routes
   (route/not-found "Not found!"))
 
 
 (def app
   (-> (handler/site main-routes)
-      (tower.ring/wrap-tower-middleware :fallback-locale :en :tconfig translate/config)
+      (tower.ring/wrap-tower-middleware :fallback-locale :en :tconfig translate/tower-config)
       (wrap-resource "public")
       (wrap-file-info)))
