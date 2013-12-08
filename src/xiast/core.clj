f033cd67db854f2bb4363f906f9bf90f22a1d976
Adriaan Leijnse <adriaan@leijnse.net>
2013-12-08 19:53:41 +0100

    Schedule routes and example template added

--- src/xiast/core.clj
+++ src/xiast/core.clj
@@ -54,11 +54,39 @@
     (let [session (session/from-cookies cookies)]
       (assoc (resp/redirect "/") :cookies (session/to-cookies (session/kill-session! session))))))
 
+(defn- block-time->time-str [t]
+  (str (+ 7 (quot (- t 1) 2))
+       ":"
+       (if (= 0 (mod (- t 1) 2))
+         "00"
+         "30")))
+(defsnippet schedule-body "templates/schedule.html" [:div#page-content]
+  [schedule-blocks]
+  [:ul#schedule :li] (clone-for [sb schedule-blocks]
+                                (content (str "W" (:week sb)
+                                              " D" (:day sb)
+                                              " " (block-time->time-str (:start-time sb))
+                                              "-" (block-time->time-str (:end-time sb))
+                                              ": " (-> sb :course :title)
+                                              " in " (:room sb)))))
+;; FIXME, hack?
+(defn- schedule-page [schedule-blocks]
+  (base (-> (schedule-body schedule-blocks)
+            (translate/translate-nodes))))
+(defroutes schedule-routes
+  (GET "/schedule/student/:student-id" [student-id]
+       (schedule-page (query/student-schedule *mock-data* student-id)))
+  (GET "/schedule/room/:room-id" [room-id]
+       (schedule-page (query/room-schedule *mock-data* room-id)))
+  (GET "/schedule/course/:course-id" [course-id]
+       (schedule-page (query/course-schedule *mock-data* course-id))))
+
 ;;; Read: https://github.com/weavejester/compojure/wiki
 (defroutes main-routes
   index-routes
   about-routes
   login-routes
+  schedule-routes
   (route/not-found "Not found!"))
 
 
