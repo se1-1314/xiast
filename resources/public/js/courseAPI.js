443a010b7fa556eb4e500c89c8f3e60951057df4
Spartam <kwpardon@infogroep.be>
2014-04-01 11:54:27 +0200

    Comments / documentation

--- resources/public/js/courseAPI.js
+++ resources/public/js/courseAPI.js
@@ -1,5 +1,9 @@
 /*
+Name: courseAPI.js
+Creation Date: 21/03/2014
+Author: Kwinten Pardon
 
+API calls and processing of resulting JSON concerning courses
 */
 
 function list_courses(keyword){
--- resources/public/js/programAPI.js
+++ resources/public/js/programAPI.js
@@ -1,51 +1,78 @@
 /*****************************************
+Name: programAPI.js
+Creation Date: 21/03/2014
+Last modified: 1/04/2014
+Author: Kwinten Pardon
 
-
-
+API calls and processing of resulting JSON
+concerning programs
  *****************************************/
+
+/*
+Name: list_programs
+Arguments: 	divID: Required ID of the div where the programs should be listed
+			keyword: optional keyword when you are searching for a specific program
+Returns: Void
+Author: Kwinten Pardon
+Date: 01/04/2014
+*/
 function list_programs(divID, keyword){
 
 	try {
+		// If divID is empty (required parameter)
+		// We throw an error stating that divID is required
 		if (typeof divID === 'undefined') {
 			throw("Requires divID");
 		}
-		//typeof divID === 'undefined' ? throw("Requires divID");
 
+		// Given a keyword: the command should be find.
+		// otherwise the command should be list (list every existing program)
 		var command = (typeof keyword === 'undefined') ? "list" : ("find/").concat(keyword);
+		// Create API url ServerLove.js for more details
 		var url = apiprogram(command);
 
+		// The fun starts with the JSON call
+		// this is an AJAX call that executes the given function on the resulting data
 		$.getJSON(url, function (data) {
+			// We are going to store the information in an array and in the end write the array to the given div
 			var programs = [];
 
 			$.each(data, function(key, val) {
+				// We want the values correspondending the key: 'programs'
+				// We check if we have the expected key
 				if (key === 'programs'){
 
+					// variable creation
 					var id = -1
 					var title = -1
 
-					/*because strange JSON constructions makes for multiple loops*/
+					// because strange JSON constructions makes for multiple loops
 					$.each(val, function(key, val){
 						$.each(val, function(key, val){
-							console.log(key + ' - ' + val);
+							// we store the value of title in the variable 'title'
 							if (key == 'title'){
 								title = val;
 							}
+							// Same applies to program-id
 							else if (key == 'program-id'){
 								id = val;
 							}
 						})
 					})
-
+					// Store the information in the programs array
 					programs.push("<li id='" + id + "'>" + title + "</li>");
 				}
 			})
-
+			// Writing the information to the html div addressed by there given ID
 			$(divID).append("<ul id='program-list'></ul>");
 
 			$.each(programs, function(index, value) {
 				$("#program-list").append(value);
 			});
 		});
+	// If an error was throw we display the error to the console of the user.
+	// He may then choose to laugh or warn us about it.
+	// He / She will Probably do both
 	} catch(error) {
 		console.error(error);
 	}
