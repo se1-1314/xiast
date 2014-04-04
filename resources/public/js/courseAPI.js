bf73b6cfdf8564b0ce9840884bb6cde705a7d729
Spartam <kwpardon@infogroep.be>
2014-04-04 16:22:44 +0200

    Programs are working need dummy data before I can continue with course

--- resources/public/js/courseAPI.js
+++ resources/public/js/courseAPI.js
@@ -6,13 +6,62 @@ Author: Kwinten Pardon
 API calls and processing of resulting JSON concerning courses
 */
 
-function list_courses(keyword){
+function print_json(data){
+	console.log(data);
+}
 
-	var command = (typeof keyword === 'undefined') ? "list" : ("find/").concat(keyword);
-	var url = apicourse(command);
+function list_courses(divID, keyword){
 
-	$.getJSON(url, function (data){
-		console.log(data);
-	})
+	try {
+		// If divID is empty (required parameter)
+		// We throw an error stating that divID is required
+		if (typeof divID === 'undefined') {
+			throw("Requires divID");
+		}
+
+		// Given a keyword: the command should be find.
+		// otherwise the command should be list (list every existing program)
+		var command = (typeof keyword === 'undefined') ? "list" : "find";
+		// Create API url ServerLove.js for more details
+		var url = apicourse(command);
+
+		// The fun starts with the JSON call
+		// this is an AJAX call that executes the given function on the resulting data
+		if (command == 'list'){
+
+			$.ajax({
+  				type: "GET",
+  				url: url,
+  				//success: process_JSON_program(divID),
+  				success: print_json,
+  				dataType: "JSON"
+				});
+
+			} else {
+
+				var data = new Object();
+				data.keywords = [keyword];
+				data = JSON.stringify(data);
+
+				$.ajax({
+  				type: "POST",
+  				url: url,
+  				data: data, 
+  				processData: false,
+  				contentType: "application/json",
+  				//success: process_JSON_program(divID),
+  				success: print_json,
+  				dataType: "JSON",
+
+				});
+			}
+
+	// If an error was throw we display the error to the console of the user.
+	// He may then choose to laugh or warn us about it.
+	// He / She will Probably do both
+	} catch(error) {
+	//	console.log(error);
+	}
+
 
 }
