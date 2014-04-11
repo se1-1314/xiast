ab3bba1dc54ddfccc3f89447c9a8fb9785efcb10
Adriaan Leijnse <adriaan@leijnse.net>
2014-04-11 17:45:55 +0200

    Added Midje dependency

--- project.clj
+++ project.clj
@@ -9,7 +9,6 @@
                  [enlive "1.1.4"]
                  [com.taoensso/tower "2.0.1"]
                  [hiccup "1.0.4"]
-                 [clj-http "0.1.3"]
                  [prismatic/schema "0.2.1"]
                  [org.clojure/data.json "0.2.4"]
                  [org.clojure/java.jdbc "0.2.3"]
@@ -17,8 +16,10 @@
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  [mysql/mysql-connector-java "5.1.25"]
                  [org.clojure/math.combinatorics "0.0.7"]
-                 [slingshot "0.10.3"]]
-  :plugins [[lein-ring "0.8.8"]]
+                 [slingshot "0.10.3"]
+                 [midje "1.6.3"]]
+  :plugins [[lein-ring "0.8.8"]
+            [lein-midje "3.1.3"]]
   :main ^:skip-aot xiast.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
