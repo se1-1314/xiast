a65b5600be3ebb4c3e1c386b04b0ac2e42eff1e6
Youssef Boudiba <youssef.boudiba@hotmail.com>
2014-04-11 14:36:21 +0200

    Updated semi-schedulingAPI.js

--- resources/public/js/semi-schedulingAPI.js
+++ resources/public/js/semi-schedulingAPI.js
@@ -30,76 +30,72 @@ Returns: a string (or array) containing the selected facilities
 Creation Date: 10/04/2014
 !We gaan hier vanuit dat we vakken kunnen selecteren en dus de bijhorende courseid kunnen opvragen!
 */
-function get_facilities_course(divID,course-code)
+function show_facilities_course(divID)
+{
+	return function(data)
+	{
+		var facilities = [];
+		$.each(data, function(key, val) 
+		{
+			if (key === 'activities')
+			{
+				alert('activities');
+				$.each(val, function(key, val)
+				{
+					$.each(val, function(key, val)
+					{
+						if (key == 'course_facility_requirements')
+						{
+							$.each(val, function(key, val)
+							{
+								if (key == 'facilities')
+								{
+									$.each(val, function(key, val)
+									{
+										if (key == 'enum')
+										{
+											
+											$.each(val, function(key, val)
+											{
+												facilities.push("<li>" +  val + "</li>");
+											});
+										}
+									});
+								}
+							});
+						}
+					});
+				});
+			}
+		});
+				$(divID).empty();
+				$(divID).append("<ul id='facilities-list'></ul>");
+	
+				$.each(facilities, function(index, value) 
+				{
+					$("#facilities").append(value);
+				});
+	};	
+}
+
+function get_facilities_course(divID, course_code)
 {
 	try {
 			if (typeof divID === 'undefined')
 			{
 				throw("divID undefined");
 			}
-	
-			// Create API url ServerLove.js for more details
-			var url = apicourse('/get/' + course-code);
-	
+			var url = apicourse('get/' + course_code);
 			$.ajax(
 				{
 			  		type: "GET",
 			  		url: url,
 			  		success: show_facilities_course(divID),
 			  		dataType: "JSON"
-				});
-				
+				});	
 		}
 	catch(error) 
 		{
 			alert(error.message);
 		}
-}
-
-function show_facilities_course(divID)
-{
-	return function(data){
-	var facilities = [];
-	
-	$.each(data, function(key, val) 
-			{
-				if (key === 'activities')
-				{
-					$.each(val, function(key, val)
-						{
-								if (key == 'course_facility_requirements')
-								{
-									$.each(val, function(key, val)
-										{
-											if (key == 'facilities')
-												{
-													$.each(val, function(key, val)
-														{
-															if (key == 'enum')
-															{
-																$.each(val, function(key, val)
-																{
-																	facilities.push("<li>" +  val + "</li>");
-																});
-															}
-														});
-												}
-										});
-								}
-						});
-				}
-			});
-			// Writing the information to the html div addressed by there given ID
-			$(divID).empty();
-			$(divID).append("<ul id='facilities-list'></ul>");
-
-			$.each(facilities, function(index, value) 
-			{
-				$("#facilities").append(value);
-			});
-}		
-$("#test").click(function()
-	{
-		$("#schedule-content").fadeIn('slow').fadeOut('slow');
-		get_facilities_course('facilities',007);
-	});
+}	
