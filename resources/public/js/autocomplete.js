2e208cb837b1a4fd69177008c7941601bfcba83d
Lars VH <lavholsb@vub.ac.be>
2014-05-05 17:35:31 +0200

    added file for autocomplete stuff

---
+++ resources/public/js/autocomplete.js
@@ -0,0 +1,11 @@
+// Converts array of raw programs to an array of strings:
+// ["program_title -- program_id", ...].
+// To be used in auto-complete list when looking for
+// schedule of a specific program
+function get_program_titles_ids(raw_programs){
+    var titles_ids = new Array();
+    for(var i = 0; i < raw_programs.length; i++){
+       titles_ids[i] = raw_programs[i].title + " -- " + raw_programs[i].id;
+    }
+return titles_ids;
+}
