8ca056235c6e1f89c79e3a56d794c0e104a6e010
Spartam <kwpardon@infogroep.be>
2014-05-06 11:58:36 +0200

    make sure the functions you are using are imported, .. updated jquery-ui.js to actually include auto-complete functionalities

--- resources/public/js/classroom-editAPI.js
+++ resources/public/js/classroom-editAPI.js
@@ -10,14 +10,15 @@ function get_buildings() {
 			result =  data;
 		}
 	});
-	result = data;
+	console.log(result);
+	return result;
 }
 
 function construct_available_tags() {
 	var json_data = get_buildings();
 	var available_tags = [];
 	$.each(json_data, function(key) {
-		alert(key);
+		console.log(key);
 		available_tags.push(key);
 	});
 	return available_tags;
@@ -25,7 +26,7 @@ function construct_available_tags() {
 
 $(function() {
 	var availableTags = construct_available_tags();
-	alert(availableTags);
+	console.log(availableTags);
 	$("#keyword-building").autocomplete({
 		source : availableTags
 	});
