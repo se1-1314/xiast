c8b4b7e880531a6a5407f932f32757bb8f98989e
Nils Van Geele <nils.vgeele@gmail.com>
2014-05-15 15:41:34 +0200

    Made the dictionary file not look horrendous

--- resources/dictionaries/all.clj
+++ resources/dictionaries/all.clj
@@ -1,6 +1,7 @@
 {:nl-BE
  {:index {:welcome "Welkom bij Xiast, %s"
-          :description "Een lessenplanner voor studenten, titularissen en faculteitbeheerders van de VUB"}
+          :description "Een lessenplanner voor studenten, titularissen
+          en faculteitbeheerders van de VUB"}
   :header {:home "Home"
            :about "About"
            :courses "Vakken"
@@ -8,16 +9,33 @@
            :login "Inloggen"}
   :about {:team "Het Xiast team"
           :about {:title "Over Xiast"
-                  :content1 "Xiast (Xiast is a scheduling tool) is een webplatform met ondersteuning voor android toestellen voor het weergeven en genereren van uurroosters. Deze is geîmplementeerd door studenten computerwetenschappen van de VUB (Vrije Universiteit Brussel)."
-		  :content2 "Deze applicatie maakt het mogelijk om een persoonlijk rooster op te stellen uit vrij te kiezen vakken. Veranderingen gemaakt door de lesgevers (bv. les afgelast, lokaalswijziging)  worden automatisch door Xiast meegedeeld en behandeld. Hiernaast kunnen de gebruikers vrije lokalen opzoeken die aan bepaalde vereisten voldoen."
-		  :content3 "Hoewel Xiast specifiek gemaakt is voor studenten en personeel van de VUB, kan iedereen Xiast gebruiken. Gebruikers zonder een VUB-account kunnen zich inloggen als guest en zo de uurrooster raadplegen of zelf een samenstellen."
-                  }
+                  :content1 "Xiast (Xiast is a scheduling tool) is een
+                  webplatform met ondersteuning voor android
+                  toestellen voor het weergeven en genereren van
+                  uurroosters. Deze is geîmplementeerd door studenten
+                  computerwetenschappen van de VUB (Vrije Universiteit
+                  Brussel)."
+		  :content2 "Deze applicatie maakt het mogelijk om een
+		  persoonlijk rooster op te stellen uit vrij te kiezen
+		  vakken. Veranderingen gemaakt door de
+		  lesgevers (bv. les afgelast, lokaalswijziging)
+		  worden automatisch door Xiast meegedeeld en
+		  behandeld. Hiernaast kunnen de gebruikers vrije
+		  lokalen opzoeken die aan bepaalde vereisten
+		  voldoen."
+		  :content3 "Hoewel Xiast specifiek gemaakt is voor
+                  studenten en personeel van de VUB, kan iedereen
+                  Xiast gebruiken. Gebruikers zonder een VUB-account
+                  kunnen zich inloggen als guest en zo de uurrooster
+                  raadplegen of zelf een samenstellen."  }
           :info {:title "Bijkomende informatie"
-                 :content1 "Alle documenten (source code, documentatie, etc.) zijn publiek beschikbaar op Github. Documenten worden bewaard onder xiast-docs, source code onder xiast."
-                 :content2 "Vermits Xiast een open source project is, staan we open voor alle soorten bijdragen. "
-                 :content3 "Heeft u vragen? Contacteer ons op:"
-                 }
-          }
+                 :content1 "Alle documenten (source code,
+                 documentatie, etc.) zijn publiek beschikbaar op
+                 Github. Documenten worden bewaard onder xiast-docs,
+                 source code onder xiast."
+                 :content2 "Vermits Xiast een open source project is,
+                 staan we open voor alle soorten bijdragen. "
+                 :content3 "Heeft u vragen? Contacteer ons op:"}}
   :authentication {:incorrect-credentials "Foute inloggegevens!"
                    :already-logged-in "U bent al ingelogd!"
                    :logged-in-successful "U bent ingelogd!"}
@@ -29,11 +47,11 @@
   :classroom-edit {:title "Lokalen aanpassen"
                    :buildings "Gebouwen"
                    :rooms "Lokalen"
-                   :room-details "Details lokaal"}
-  }
+                   :room-details "Details lokaal"}}
  :en
  {:index {:welcome "Welcome to Xiast, %s"
-          :description "A scheduling application for students, teachers and faculty managers of the VUB"}
+          :description "A scheduling application for students,
+          teachers and faculty managers of the VUB"}
   :header {:home "Home"
            :about "About"
            :courses "Courses"
@@ -41,16 +59,34 @@
            :login "Login"}
   :about {:team "The Xiast team"
           :about {:title "About Xiast"
-                  :content1 "Xiast (Xiast is a scheduling tool) is a webplatform with support for android devices to display and generate schedules. This application is designed and implemented by students of computer-science at the VUB (Vrije Universiteit Brussel)."
-		  :content2 "This application provides users with the ability to compose their personal schedule out of all courses with no limitation. Changes made by the tutor(E.g. Course cancelled, change of room) will be automatically brought to the attention of its registered students. Users also have the ability to search for unoccupied classrooms with specified equipment."
-		  :content3 "Although Xiast is made for students and personal of the VUB,it is accessible for everyone. People without a VUB-account are able to log in as guest and may compose their own personal schedule."
-		  }
+                  :content1 "Xiast (Xiast is a scheduling tool) is a
+                  webplatform with support for android devices to
+                  display and generate schedules. This application is
+                  designed and implemented by students of
+                  computer-science at the VUB (Vrije Universiteit
+                  Brussel)."
+		  :content2 "This application provides users with the
+		  ability to compose their personal schedule out of
+		  all courses with no limitation. Changes made by the
+		  tutor(E.g. Course cancelled, change of room) will be
+		  automatically brought to the attention of its
+		  registered students. Users also have the ability to
+		  search for unoccupied classrooms with specified
+		  equipment."
+		  :content3 "Although Xiast is made for students and
+		  personal of the VUB,it is accessible for
+		  everyone. People without a VUB-account are able to
+		  log in as guest and may compose their own personal
+		  schedule."  }
           :info {:title "Other information"
-                 :content1 "All documents, source code and other artifacts are publicly available on Github. Documents can be found under xiast-docs, source code can be found under xiast."
-                 :content2 "Since xiast is an open source project, contributions of any kind are certainly welcome!"
-                 :content3 "If you have any questions feel free to contact us at."
-                 }
-          }
+                 :content1 "All documents, source code and other
+                 artifacts are publicly available on Github. Documents
+                 can be found under xiast-docs, source code can be
+                 found under xiast."
+                 :content2 "Since xiast is an open source project,
+                 contributions of any kind are certainly welcome!"
+                 :content3 "If you have any questions feel free to
+                 contact us at."}}
   :authentication {:incorrect-credentials "Incorrect credentials!"
                    :already-logged-in "You are already logged in!"
                    :logged-in-successful "You are now logged in!"}
@@ -62,5 +98,4 @@
   :classroom-edit {:title "Room edit"
                    :buildings "Buildings"
                    :rooms "Rooms"
-                   :room-details "Details room"}
-  }}
+                   :room-details "Details room"}}}
